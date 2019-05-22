a27d74dc002e98315b5e406a634dc0dc752b0bd4
Ben Gardner <bgardner@deloitte.com>
2019-05-22 08:08:34 -0400

    Updates

---
+++ redirect/app.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const {
+  PORT = 3000,
+  REDIRECT_URL = 'https://menubar.com'
+} = process.env
+
+http.createServer((req, res) => {
+  const url = `${REDIRECT_URL}${req.url}`
+  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Location', url)
+  res.statusCode = 301
+  res.end(`Redirecting to ${url}`)
+}).listen(PORT)
